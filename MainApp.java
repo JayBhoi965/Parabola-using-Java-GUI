42e8aaa597b014dfd97c6d0f45ecb91c146eb552
Jay Bhoi <154799187+JayBhoi965@users.noreply.github.com>
2025-05-23 00:05:05 +0530

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -2,6 +2,9 @@ package com.parabola.gui;
 
 public class MainApp {
     public static void main(String[] args) { 
-       
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            GraphWindow window = new GraphWindow();
+            window.setVisible(true);
+        });
     }
 }
