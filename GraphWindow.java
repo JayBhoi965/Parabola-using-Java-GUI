a5bb612b79be2716345edcdb2925193491855a94
Jay Bhoi <154799187+JayBhoi965@users.noreply.github.com>
2025-05-23 00:06:35 +0530

    Update GraphWindow.java

--- GraphWindow.java
+++ GraphWindow.java
@@ -1,1 +1,26 @@
+package com.parabola.gui;
 
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphWindow extends JFrame {
+
+    private ParabolaPanel graphPanel;
+    private ControlPanel controlPanel;
+
+    public GraphWindow() {
+        super("Modular Parabola Plotter");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000, 700);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+ 
+        ParabolaEquation equation = new ParabolaEquation(0.01, 0, 0);
+        graphPanel = new ParabolaPanel(equation);
+        controlPanel = new ControlPanel(equation, graphPanel);
+
+        add(controlPanel, BorderLayout.NORTH);
+        add(graphPanel, BorderLayout.CENTER);
+    }
+}
